03008fffbe080ab1eb34fa48f3ae4a944e2b915a
Marx Gabas <bgabasmarxnathaniel@gmail.com>
2026-09-09 10:00:00 +0800

    minor fixes

---
+++ js/supabase.config.js
@@ -0,0 +1,4 @@
+export const SUPABASE_URL = "https://qscvccklksptcvtyzabe.supabase.co";
+export const SUPABASE_KEY = "sb_publishable_cyj-6CYVmr2MHsj1CDhowQ_O30bZsIg";
+export const ANTHROPIC_KEY = "sk-ant-api03-...";
+export const GROQ_KEY = "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";
